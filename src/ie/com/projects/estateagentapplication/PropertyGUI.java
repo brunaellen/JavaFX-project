1f3c03a5e4e0ba1f02f8f73d6487e5b5163ac9b5
brunaellen <brunaellen.souza@hotmail.com>
2021-08-28 22:42:59 +0100

    Adding correct files

---
+++ src/ie/com/projects/estateagentapplication/PropertyGUI.java
@@ -0,0 +1,68 @@
+package ie.com.projects.estateagentapplication;/*
+Bruna Ellen Gurgel Souza - L00157216
+Assignment1 (create a GUI)
+*/
+//import packages containing predefined classes
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+//PropertyGUI is the main Property GUI class
+
+//main class
+public class PropertyGUI extends Application
+{
+   //declare and instantiate PropertyDataStore object
+   PropertyDataStore propertyDataStore = new PropertyDataStore();
+   //declare and instantiate TextFieldValidationUtils object
+   TextFieldValidationUtils textFieldValidationUtils = new TextFieldValidationUtils();
+   //declare and instantiate PropertyCustomerAreaGUI object
+   PropertyCustomerAreaGUI customerArea = new PropertyCustomerAreaGUI(propertyDataStore);
+   //declare and instantiate PropertyStaffAreaGUI object
+   PropertyStaffAreaGUI staffArea = new PropertyStaffAreaGUI(propertyDataStore);
+   
+   //start method
+   public void start(Stage stage)
+   {  /*Scene object(customerScene) will receive customer scene.
+      customerArea.getScene() method will get customer scene 
+      from PropertyCustomerAreaGUI object*/
+      Scene customerScene = customerArea.getScene();
+      //Add customer scene to the stage
+      stage.setScene(customerScene);
+      //set title of stage
+      stage.setTitle("LYIT Estate Agents");
+      //Show the stage
+      stage.show();
+      
+      /*Scene object(staffScene) will receive staff scene.
+      staffArea.getStaffScene() method will get staff scene 
+      from PropertyStaffAreaGUI object*/
+      Scene staffScene = staffArea.getStaffScene();
+      /*Scene object(updatePropScene) will receive "Update Property" scene.
+      staffArea.getUpdatePropScene() method will get "Update Property" scene 
+      from PropertyStaffAreaGUI object*/   
+      Scene updatePropScene = staffArea.getUpdatePropScene();
+      
+      /*using lambda expression to change from one scene to another scene
+       when a specific button is pressed*/
+       //when button getBtnStaffArea is pressed, display staff scene.
+      customerArea.getBtnStaffArea().setOnAction(e -> {
+         stage.setScene(staffScene);//display staffScene
+         customerArea.clearTxtCustomerOutput();//clear customer text area
+      });
+      //when button btnReturnCustomerPage is pressed, display customer scene.
+      staffArea.getBtnReturnCustomerPage().setOnAction(e -> stage.setScene(customerScene));
+      //when button btnUpdateAndDeleteArea is pressed, display "Update Property" scene.
+      staffArea.getBtnUpdateAndDeleteArea().setOnAction(e -> stage.setScene(updatePropScene));
+      //when button btnReturnStaffPage is pressed, display staff scene.
+      staffArea.getBtnReturnStaffPage().setOnAction(e -> stage.setScene(staffScene));
+      
+   }//end start method
+   
+   //main method
+   public static void main(String[]args)
+   {
+      launch(args);
+   }//end of main method
+}//end of class
